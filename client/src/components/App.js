f234b7b85c0c340efd4ec67bcc8cffb17d6fe59b
Ryan <salvat36@gmail.com>
2023-07-04 13:08:16 -0500

    setup user state, handler func and fetchUser

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,26 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
-}
+  const [user, setUser] = useState(null)
 
+  const updateUser = (user) => {
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const fetchUser = () => {
+      fetch('/authenticate')
+        .then((res) => {
+          if (res.ok) {
+            res.json().then(updateUser);
+          } else {
+            updateUser(null);
+          }
+          })
+    }
+    fetchUser();
+  }, []);
+
+}
 export default App;
