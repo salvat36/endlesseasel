7bd97f5bd5a74ac72743b1b2e19f07b0d2e8834d
Ryan <salvat36@gmail.com>
2023-07-03 21:50:48 -0500

    setup model import and user model

--- server/models.py
+++ server/models.py
@@ -1,5 +1,32 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    _password_hash= db.Column(db.String)
+    email= db.Column(db.String)
+    cart= db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password.encode('utf-8'))
+    
+    created_at = db.Column(db.DateTime, server_default= db.func.now())
+    updated_at = db.Column(db.DateTie, onupdate=db.func.now())
