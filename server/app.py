5c14a18578c8e6f7a00de1a422bf3bb593270bed
Ryan <salvat36@gmail.com>
2023-07-04 08:21:52 -0500

    setup delete route for users and add env key

--- server/app.py
+++ server/app.py
@@ -11,18 +11,22 @@ from flask import Flask, request, make_response, session
 # Local imports
 from config import app, db, api
 from models import User, Review, Artwork, UserArtwork
+
 # Views go here!
 
+
 # Home Route
-@app.route('/')
+@app.route("/")
 def home():
-    return '<h1>EndlessEasel</h1>'
+    return "<h1>EndlessEasel</h1>"
+
 
 class Users(Resource):
     def get(self):
         users = [a.to_dict() for a in User.query.all()]
         return make_response(users, 200)
-    
+
+
 #! Need to add secret key to get this POST working
 #     def post(self):
 #         data = request.get_json()
@@ -34,66 +38,93 @@ class Users(Resource):
 #             return make_response(new_user.to_dict(), 201)
 #         except Exception as e:
 #             return make_response({'errors': [str(e)]}, 400)
-api.add_resource(Users, '/users')
+api.add_resource(Users, "/users")
+
 
 class UserById(Resource):
     def get(self, id):
         user = db.session.get(User, id)
         if user:
             return make_response(user.to_dict(), 200)
-        return make_response({'error': 'User not found'}, 404)
-api.add_resource(UserById, '/users/<int:id>')
+        return make_response({"error": "User ID not found"}, 404)
 
-#Views for ALL Artworks
+    def delete(self, id):
+        user = db.session.get(User, id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return make_response("", 200)
+        return make_response({"error": "User ID not found"})
+
+
+api.add_resource(UserById, "/users/<int:id>")
+
+
+# Views for ALL Artworks
 class Artworks(Resource):
     def get(self):
         artworks = [a.to_dict() for a in Artwork.query.all()]
         return make_response(artworks, 200)
-api.add_resource(Artworks, '/artworks')
 
-#Views for ONE Artwork
+
+api.add_resource(Artworks, "/artworks")
+
+
+# Views for ONE Artwork
 class ArtworkById(Resource):
     def get(self, id):
         artwork = db.session.get(Artwork, id)
         if artwork:
             return make_response(artwork.to_dict(), 200)
-        return make_response({'error': 'Artwork not found'}, 404)
-api.add_resource(ArtworkById, '/artworks/<int:id>')
+        return make_response({"error": "Artwork not found"}, 404)
 
-#View for ALL UserArtworks
+
+api.add_resource(ArtworkById, "/artworks/<int:id>")
+
+
+# View for ALL UserArtworks
 class UserArtworks(Resource):
     def get(self):
         user_artworks = [ua.to_dict() for ua in UserArtwork.query.all()]
         return make_response(user_artworks, 200)
-api.add_resource(UserArtworks, '/user-artworks')
 
-#View for ONE UserArtwork
+
+api.add_resource(UserArtworks, "/user-artworks")
+
+
+# View for ONE UserArtwork
 class UserArtworkById(Resource):
     def get(self, id):
         user_artwork = db.session.get(UserArtwork, id)
         if user_artwork:
             return make_response(user_artwork.to_dict(), 200)
-        return make_response({'error': 'UserArtwork not found'}, 404)
-api.add_resource(UserArtworkById, '/user-artworks/<int:id>')
+        return make_response({"error": "UserArtwork not found"}, 404)
 
-#View for ALL Reviews
+
+api.add_resource(UserArtworkById, "/user-artworks/<int:id>")
+
+
+# View for ALL Reviews
 class Reviews(Resource):
     def get(self):
         reviews = [r.to_dict() for r in Review.query.all()]
         return make_response(reviews, 200)
-api.add_resource(Reviews, '/reviews')
 
+
+api.add_resource(Reviews, "/reviews")
+
+
+# View for ONE Review
 class ReviewById(Resource):
     def get(self, id):
         review = db.session.get(Review, id)
         if review:
             return make_response(review.to_dict(), 200)
-        return make_response({'error': 'Review not found'}, 404)
-api.add_resource(ReviewById, '/reviews/<int:id>')
+        return make_response({"error": "Review not found"}, 404)
 
 
+api.add_resource(ReviewById, "/reviews/<int:id>")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True, use_debugger=False, use_reloader=False)
--- server/config.py
+++ server/config.py
@@ -8,14 +8,19 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
+from os import environ
+from dotenv import load_dotenv
 
 # Local imports
+
 
 # Instantiate app, set attributes
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+load_dotenv('.env')
+app.secret_key = environ.get('SECRET_KEY')
 
 # Define metadata, instantiate db
 metadata = MetaData(naming_convention={
