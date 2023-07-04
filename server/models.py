3906a4b74d1cafa591b502ad96d17b219c8c62b8
Ryan <salvat36@gmail.com>
2023-07-03 22:03:54 -0500

    setup artwork Model

--- server/models.py
+++ server/models.py
@@ -5,28 +5,43 @@ from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
-# Models go here!
 
+# User Model
 class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
-    _password_hash= db.Column(db.String)
-    email= db.Column(db.String)
-    cart= db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    email = db.Column(db.String)
+    cart = db.Column(db.String)
 
+    # User Password_Hash
     @hybrid_property
     def password_hash(self):
         return self._password_hash
-    
+
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
 
     def authenticate(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password.encode('utf-8'))
-    
-    created_at = db.Column(db.DateTime, server_default= db.func.now())
+        return bcrypt.check_password_hash(self.password_hash, password.encode("utf-8"))
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTie, onupdate=db.func.now())
+
+
+# Artwork Model
+class Artwork(db.Model, SerializerMixin):
+    __tablename__ = "artworks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    genre = db.Column(db.String)
+    price = db.Column(db.Float)
+    title = db.Column(db.String)
+    image = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
