a22a9bd838407c5a8f394d6b24472357324a54fb
Ryan <salvat36@gmail.com>
2023-07-17 17:55:27 -0500

    style error component using mui snackbar

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,32 +1,44 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ErrorContext } from "../context/ErrorProvider";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const Error = () => {
-  const { error } = useContext(ErrorContext);
-  const [visible, setVisible] = useState(null);
+  const { error, setError } = useContext(ErrorContext);
+  const [open, setOpen] = useState(null);
 
-  // const handleVisibility = () =>{
-  //   setVisible(current => !current)
-  // }
-
-  useEffect(()=> {
+  useEffect(() => {
     if (error) {
-      setVisible(true);
-      const timer = setTimeout(()=> {
-        setVisible(false);
-    }, 3000);
+      setOpen(true);
+      const timer = setTimeout(() => {
+        setError(null);
+      }, 3000);
 
-    return () => {
-      clearTimeout(timer);
+      return () => {
+        clearTimeout(timer);
+      };
     }
-  }
-  }, [error]);
+  }, [error, setError]);
 
-  return visible && (
-    <div>
-      <p>{error}</p>
-    </div>
-  ) 
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+        {error}
+      </Alert>
+    </Snackbar>
+  );
 };
 
 export default Error;
