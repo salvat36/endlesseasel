bfa00a0471d77c3a27bd526bed5a699f9fa3e40e
Ryan <salvat36@gmail.com>
2023-07-17 16:23:45 -0500

    move errorprovider and userprovider to index.js so app can access context

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,12 +8,11 @@ import Register from "./Register";
 import ArtDetail from "./ArtDetail";
 import Create from "./Create";
 import UserArtwork from "./UserArtwork";
-import UserProvider from "../context/UserProvider";
-import ErrorProvider from "../context/ErrorProvider";
-
+import { ErrorContext } from "../context/ErrorProvider";
 
 function App() {
   const [artworks, setArtworks] = useState([]);
+  const { setError } = useContext(ErrorContext);
 
   const updateArtworks = (artworks) => {
     setArtworks(artworks);
@@ -23,39 +22,32 @@ function App() {
     fetch("/artworks")
       .then((res) => res.json())
       .then(setArtworks)
-      .catch((err) => console.log(err));
-  }, []);
+      .catch((error) => setError(error));
+  }, [setError]);
 
   return (
     <main>
       <Switch>
-        <ErrorProvider>
-          <UserProvider>
-            <Navigation />
-            <Register />
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/shop">
-              <Shop artworks={artworks} updateArtworks={updateArtworks} />
-            </Route>
-            <Route path="/user-artworks">
-              <UserArtwork
-                updateArtworks={updateArtworks}
-                artworks={artworks}
-              />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/artworks/:id">
-              <ArtDetail />
-            </Route>
-            <Route path ='/create'>
-              <Create updateArtworks={updateArtworks}/>
-            </Route>
-          </UserProvider>
-        </ErrorProvider>
+        <Navigation />
+        <Register />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/shop">
+          <Shop artworks={artworks} updateArtworks={updateArtworks} />
+        </Route>
+        <Route path="/user-artworks">
+          <UserArtwork updateArtworks={updateArtworks} artworks={artworks} />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/artworks/:id">
+          <ArtDetail />
+        </Route>
+        <Route path="/create">
+          <Create updateArtworks={updateArtworks} />
+        </Route>
       </Switch>
     </main>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App.js";
+import UserProvider from "../context/UserProvider";
+import ErrorProvider from "../context/ErrorProvider";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ErrorProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </ErrorProvider>
   </Router>,
   document.getElementById("root")
 );
