698982a1d26e081f2f17e7fa0c5bd1ae9a3c7983
Ryan <salvat36@gmail.com>
2023-07-18 02:27:41 -0500

    complete basic navbar links

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,21 +1,121 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { UserContext } from '../context/UserProvider'
+import { UserContext } from "../context/UserProvider";
 import { useHistory } from "react-router-dom";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 
 const Navigation = () => {
-  const history = useHistory()
-  const { handleLogoutClick, user } = useContext(UserContext)
+  const history = useHistory();
+  const { handleLogoutClick, user } = useContext(UserContext);
   return (
-    <>
-    <div>TESTING</div>
-      {user && <Link onClick={handleLogoutClick}>Logout</Link>} 
-      <Link to="/"> Home </Link>
-      {user && <Link to="/shop"> Explore </Link>}
-      {user && <Link to="/user-artworks"> Personal Portfolio </Link>}
-      {user && <Link to="/create"> Create Image</Link>}
-      <Link to="/contact"> Contact Us</Link>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            EndLessEasel
+          </Typography>
+
+          <Link to="/">
+            <Button
+              sx={{
+                color: "white",
+                textDecoration: "none",
+                padding: "8px 16px",
+                margin: "0 4px",
+                borderRadius: "4px",
+                backgroundColor: "#f50057",
+              }}
+            >
+              Home
+            </Button>
+          </Link>
+
+          {user && (
+            <Link to="/shop">
+              <Button
+                sx={{
+                  color: "white",
+                  textDecoration: "none",
+                  padding: "8px 16px",
+                  margin: "0 4px",
+                  borderRadius: "4px",
+                  backgroundColor: "#f50057",
+                }}
+              >
+                Explore
+              </Button>
+            </Link>
+          )}
+
+          {user && (
+            <Link to="/user-artworks">
+              <Button
+                sx={{
+                  color: "white",
+                  textDecoration: "none",
+                  padding: "8px 16px",
+                  margin: "0 4px",
+                  borderRadius: "4px",
+                  backgroundColor: "#f50057",
+                }}
+              >
+                Portfolio
+              </Button>
+            </Link>
+          )}
+
+          {user && (
+            <Link to="/create">
+              <Button
+                sx={{
+                  color: "white",
+                  textDecoration: "none",
+                  padding: "8px 16px",
+                  margin: "0 4px",
+                  borderRadius: "4px",
+                  backgroundColor: "#f50057",
+                }}
+              >
+                Create
+              </Button>
+            </Link>
+          )}
+
+          <Link to="/contact">
+            <Button
+              sx={{
+                color: "white",
+                textDecoration: "none",
+                padding: "8px 16px",
+                margin: "0 4px",
+                borderRadius: "4px",
+                backgroundColor: "#f50057",
+              }}
+            >
+              Home
+            </Button>
+          </Link>
+
+          {user && (
+            <Link>
+              <Button
+                onClick={handleLogoutClick}
+                sx={{
+                  color: "white",
+                  textDecoration: "none",
+                  padding: "8px 16px",
+                  margin: "0 4px",
+                  borderRadius: "4px",
+                  backgroundColor: "#f50057",
+                }}
+              >
+                Logout
+              </Button>
+            </Link>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
