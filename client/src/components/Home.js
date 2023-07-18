ce5f77c76e54194e7498ed7705ce5bfb163fecd5
Ryan <salvat36@gmail.com>
2023-07-18 04:51:13 -0500

    create custom theme and add wrapper to index.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <h1>EndlessEasel</h1>
+      <h1>Welcome Home Artists and Art Lovers!</h1>
       <img
         src="https://images.nightcafe.studio/jobs/YbfjF0xAPTQHTkbeCSCU/YbfjF0xAPTQHTkbeCSCU--1--1iy5g.jpg?tr=w-828,c-at_max"
         alt="How to Video: for navigating site features"
